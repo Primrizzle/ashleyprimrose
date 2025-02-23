c80b101b1c3527c94078b5cc9c1cc62abace1480
Primrizzle <116143986+Primrizzle@users.noreply.github.com>
2025-02-22 22:40:14 -0600

    Portfolio v4.0.0

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+export default function App() { 
+  return (
+    <div className="bg-gray-900 text-white h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Ashley Primrose Software Engineer</h1>
+    </div>
+  );
+}
+
