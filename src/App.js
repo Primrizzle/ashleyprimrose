ac92f3c2e2ed303234b34cbfa9dd7de0ef43ceea
Primrizzle <116143986+Primrizzle@users.noreply.github.com>
2025-02-22 23:25:33 -0600

    New components added and background images added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 export default function App() { 
   return (
-    <div className="bg-gray-900 text-white h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Ashley Primrose Software Engineer</h1>
-    </div>
+    <>
+      <h1 className="text-4xl text-center" >Hello
+        <span>World</span>
+      </h1>
+    </>
+    
   );
 }
 
